089eefb470eca642adbf6c61c192b317d78d4180
martin <MartinChrisPC@hotmail.com>
2023-04-22 20:19:33 -0500

    1era modificacion

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,83 +8,73 @@ const resultado = document.querySelector(".texto-resultado");
 const texto = document.querySelector(".area");
 
 const valores = {
-  "e": "enter",
-  "i": "imes",
-  "a": "ai",
-  "o": "ober",
-  "u": "ufat"
-}
+  e: "enter",
+  i: "imes",
+  a: "ai",
+  o: "ober",
+  u: "ufat",
+};
 
 botonEncriptar.onclick = encriptar;
 botonDesencriptar.onclick = desencriptar;
 botonCopiar.onclick = copiarTexto;
 
 function encriptar() {
-
   let mensaje = recuperarTexto();
-  if (mensaje === "") {} else
-    ocultarDerecha();
+  if (mensaje === "") {
+    removerOcultar();
+  } else ocultarDerecha();
   resultado.textContent = encriptarTexto(mensaje);
-
 }
 
 function desencriptar() {
-
   let mensaje = recuperarTexto();
-  if (mensaje === "") {} else
-    ocultarDerecha()
+  if (mensaje === "") {
+    removerOcultar();
+  } else ocultarDerecha();
   resultado.textContent = desencriptarTexto(mensaje);
-
 }
 
 function recuperarTexto() {
-
   let mensaje = texto.value;
   return mensaje;
-
 }
 
 function encriptarTexto(texto) {
-
   let textoEncriptado = texto;
 
   for (const key in valores) {
-
     const regex = new RegExp(key, "g");
-    textoEncriptado = textoEncriptado.replace(regex, valores[key])
-
+    textoEncriptado = textoEncriptado.replace(regex, valores[key]);
   }
 
   return textoEncriptado;
-
 }
 
 function desencriptarTexto(texto) {
-
   let textoDesencriptado = texto;
 
   for (const key in valores) {
-
     const regex = new RegExp(valores[key], "g");
     textoDesencriptado = textoDesencriptado.replace(regex, key);
-
   }
 
   return textoDesencriptado;
-
 }
 
 function copiarTexto() {
-
   navigator.clipboard.writeText(resultado.innerHTML);
-  alert("Mensaje Copiado al Portapapeles")
-
+  alert("Mensaje Copiado al Portapapeles");
 }
 
 function ocultarDerecha() {
-
   munieco.classList.add("ocultar");
   h3.classList.add("ocultar");
   parrafo.classList.add("ocultar");
+}
 
+function removerOcultar() {
+  munieco.classList.remove("ocultar");
+  h3.classList.remove("ocultar");
+  parrafo.classList.remove("ocultar");
 }
